de7768fd618f7a564389c4c464e17ddbbeef63d2
Vasiuchenko <vasiuchenko@icloud.com>
2021-06-22 17:52:42 +0300

    add accordeon

--- app.js
+++ app.js
@@ -5,15 +5,65 @@
 */
 
 window.onload = () => {
-    console.log('app started.', DATABASE);
-    const PLP = document.getElementById('container');
-    let template = '';
+  console.log('app started.', DATABASE);
+  const PLP = document.getElementById('container');
+  let template = '';
 
-    DATABASE.forEach(item => {
+  showItems(DATABASE);
+  
+  let select = document.getElementById('filterBy'),
+    minus = PLP.querySelector('.js-button-minus'),
+    plus = PLP.querySelector('.js-button-plus'),
+    content = PLP.getElementsByClassName('plp__content--quantity');
+  
+  count(content);
+  
+  select.addEventListener('click', e => {
+    DATABASE.sort(sortObjectByField(select.value));
+    showItems(DATABASE);
+  });
+  
+  function count(arr) {
+    for (let item of content) {
+      let counter = 0,
+          quantity = item.querySelector('.js-selector-quantity');
+      
+      quantity.innerText = counter;
+      item.addEventListener('click', e => {
+        if (e.target.innerText === minus.innerText) {
+          counter -= 1;
+        }
+        if (e.target.innerText === plus.innerText) {
+          counter += 1;
+        }
+        if (counter < 0) {
+          counter = 0;
+        }
+        return quantity.innerText = counter;
+      })
+    }
+  }
+  
+  function showItems(arr) {
+    template = '';
+    arr.forEach(item => {
       let element = new Render('template', item);
       template += element.render();
     });
     PLP.innerHTML = template;
+  }
+
+  function sortObjectByField(field) {
+    return function (a, b) {
+      if (a[field] > b[field]) {
+        return 1;
+      }
+      if (a[field] < b[field]) {
+        return -1;
+      }
+      return 0;
+    };
+  }
 };
 
 class Render {
